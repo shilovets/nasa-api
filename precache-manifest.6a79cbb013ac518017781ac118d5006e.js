d206b881435654fbf1c48a04b4c3885e2fced7d6
Alina Shilovets <alina.shilovets@gmail.com>
2020-06-21 17:45:53 +0300

    Updates

---
+++ precache-manifest.6a79cbb013ac518017781ac118d5006e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d866d1ff2a62e95a9b027c847ef33d",
+    "url": "/nasa-api/index.html"
+  },
+  {
+    "revision": "22684ba8fc9f64e80392",
+    "url": "/nasa-api/static/css/main.24a4ed4d.chunk.css"
+  },
+  {
+    "revision": "208ab5a06a86ce4642b6",
+    "url": "/nasa-api/static/js/2.a2e720e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa-api/static/js/2.a2e720e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22684ba8fc9f64e80392",
+    "url": "/nasa-api/static/js/main.a76a4478.chunk.js"
+  },
+  {
+    "revision": "62c0038090ab73b8aeb6",
+    "url": "/nasa-api/static/js/runtime-main.900dcfc7.js"
+  }
+]);
